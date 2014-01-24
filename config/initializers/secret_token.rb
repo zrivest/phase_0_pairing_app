c0d66b537f74874956c8e088547a1095d680ad94
Zach <zrivest@gmail.com>
2014-01-24 16:02:52 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Phase0Pairing::Application.config.secret_key_base = '9929091aa75e3bec6b8a63e89c340618781e330864d0470092253fdadc952e9b028f9b9469e92c71dd19a8236c8e013a123c94cc171c5a327060f09640fe2eec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Phase0Pairing::Application.config.session_store :cookie_store, key: '_phase_0_pairing_session'
